d5299410735a917105c02b0c676f5c660ca655db
hannupi <varahahmo1@gmail.com>
2024-10-26 13:37:30 +0300

    ignore case with env var

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use std::error::Error;
-use std::fs;
+use std::{env, fs};
 
 pub struct Config {
     query: String,
     filepath: String,
+    ignore_case: bool,
 }
 
 impl Config {
@@ -14,16 +15,25 @@ impl Config {
         let query = args[1].clone();
         let filepath = args[2].clone();
 
-        Ok(Config { query, filepath })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config { query, filepath, ignore_case })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(config.filepath)?;
 
-    for line in search(&config.query, &content) {
+    let result = if config.ignore_case {
+        search_case_insensitive(&config.query, &content)
+    } else {
+        search(&config.query, &content)
+    };
+
+    for line in result {
         println!("{}", line);
     }
+
     Ok(())
 }
 
