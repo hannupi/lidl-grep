b7bd5695a099c458229c2a0a22aea0fac1e7de63
hannupi <varahahmo1@gmail.com>
2024-10-26 13:40:36 +0300

    use stderr instead of stdout

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::setup(&args).unwrap_or_else(|err| {
-        println!("Failed to parse args: {err}");
+        eprintln!("Failed to parse args: {err}");
         process::exit(1);
     });
 
     if let Err(e) = lidl_grep::run(config) {
-        println!("Error: {e}");
+        eprintln!("Error: {e}");
         process::exit(1);
     }
 }
